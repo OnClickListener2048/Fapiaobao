9e5095470d740751e12fee72cef7e89ba403cdeb
John Doe <johndoe@example.com>
2017-11-21 09:32:33 +0800

    2017-11-20 21.30

--- app/src/main/java/com/pilipa/fapiaobao/net/bean/publish/InvoiceBean.java
+++
@@ -1,8 +0,0 @@
-package com.pilipa.fapiaobao.net.bean.publish;
-
-/**
- * Created by edz on 2017/11/2.
- */
-
-public class InvoiceBean {
-}
--- app/src/main/java/com/pilipa/fapiaobao/net/bean/publish/InvoiceType.java
+++
@@ -1,9 +0,0 @@
-package com.pilipa.fapiaobao.net.bean.publish;
-
-/**
- * Created by edz on 2017/10/31.
- */
-
-public class InvoiceType {
-
-}
